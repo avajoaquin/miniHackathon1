d58b4b67a7275d7ad35507bb326ea31d1321ffc6
Ava Victoria Joaquin <avajoaquin@berkeley.edu>
2024-01-09 11:45:18 -0800

    starting point maybe?

---
+++ Company.java
@@ -0,0 +1,10 @@
+public class Company {
+    private String name;
+    public Company(String name) {
+        this.name = name;
+    }
+
+    public String getname() {
+        return this.name;
+    }
+}
